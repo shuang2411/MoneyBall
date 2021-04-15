e335150a71c47e2e8ea9a36243d0dc078a0915f4
Zhongman Zhang <zhongman.zhang@gmail.com>
2021-04-15 10:55:44 -0400

    1234

--- ant_demo/src/pages/cardInventory/index.tsx
+++ ant_demo/src/pages/cardInventory/index.tsx
@@ -1,19 +1,6 @@
 import ProTable from '@ant-design/pro-table';
-import { Button, message } from 'antd';
 import { db } from "@/services/ant-design-pro/firebase";
-import React, { useState } from 'react';
-import ProForm, {
-    ModalForm,
-    ProFormText,
-  } from '@ant-design/pro-form';
-
-const waitTime = (time: number = 100) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(true);
-      }, time);
-    });
-  };
+import NewFacilityForm from "@/components/NewFacilityForm"
 
 type Card = {
     key: number;
@@ -27,8 +14,6 @@ type Card = {
 } 
 
 const CardInventory = () => {
-    const [createModalVisible, handleModalVisible] = useState<boolean>(false);
-    const [card_uid, handleCardUid] = useState<string>("");
     const columns = [
         {
             title: 'Card ID',
@@ -65,28 +50,15 @@ const CardInventory = () => {
             dataIndex: 'option',
             key: 'option',
             render: (_, record) => [
-                <Button type="primary"
+                <a 
                     onClick = {async () => {
                         console.log(record);
                         await buyCard(record.card_doc_ref);
-                        await waitTime(1000);
-                        message.success("Order submitted");
                         location = location;
                     }}
                     key='buy'>
                     Buy
-                </Button>,
-                <Button type="primary"
-                    onClick = {async () => {
-                        console.log(record);
-                        handleModalVisible(true);
-                        handleCardUid(record.card_doc_ref);
-                        // await test(record.card_doc_ref, sellAmount, sellPrice);
-                        // await sellCard(record.card_doc_ref, sellAmount, sellPrice);
-                }}
-                key='sell'>
-                Sell
-            </Button>,
+                </a>,
             ],
         }
 
@@ -168,27 +140,13 @@ const CardInventory = () => {
             })
     }
 
-    
-    async function sellCard(card_uid, sellAmount, sellPrice) {
-        await db.collection('cards')
-            .doc(card_uid)
-            .get()
-            .then(async(querySnapshot) => {
-                if(querySnapshot.empty){
-                    console.log("Cannot find the card in user inventory");
-                    return;
-                }
-                const oldCount = querySnapshot.data().Count;
-                await db.collection('cards').doc(card_uid).update({'Count': Number(oldCount) + Number(sellAmount)});
-                console.log(sellPrice);
-                await waitTime(5000);
-            });
-    }
 
     return(
-        <div className = "cards">
         <ProTable<Card>
             columns = {columns}
+            toolBarRender={() => [
+                <NewFacilityForm/>
+              ]}
             request = { async (params = {}) => {
                     await getData();
                     console.log("All Cards: ", cards)
@@ -200,8 +158,11 @@ const CardInventory = () => {
             }
             search={false}
         ></ProTable>
+<<<<<<< HEAD
       
     </div>
+=======
+>>>>>>> parent of 20f52b4... sell with number of cards and price
     )
 };
 
