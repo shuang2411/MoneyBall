20f52b4a7bc0ac6c0706685179d53325e907267a
JeffZhou215 <yifuz@umich.edu>
2021-04-15 01:08:22 -0400

    sell with number of cards and price

--- ant_demo/src/pages/cardInventory/index.tsx
+++ ant_demo/src/pages/cardInventory/index.tsx
@@ -1,6 +1,19 @@
 import ProTable from '@ant-design/pro-table';
+import { Button, message } from 'antd';
 import { db } from "@/services/ant-design-pro/firebase";
-import NewFacilityForm from "@/components/NewFacilityForm"
+import React, { useState } from 'react';
+import ProForm, {
+    ModalForm,
+    ProFormText,
+  } from '@ant-design/pro-form';
+
+const waitTime = (time: number = 100) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, time);
+    });
+  };
 
 type Card = {
     key: number;
@@ -14,6 +27,8 @@ type Card = {
 } 
 
 const CardInventory = () => {
+    const [createModalVisible, handleModalVisible] = useState<boolean>(false);
+    const [card_uid, handleCardUid] = useState<string>("");
     const columns = [
         {
             title: 'Card ID',
@@ -50,15 +65,28 @@ const CardInventory = () => {
             dataIndex: 'option',
             key: 'option',
             render: (_, record) => [
-                <a 
+                <Button type="primary"
                     onClick = {async () => {
                         console.log(record);
                         await buyCard(record.card_doc_ref);
+                        await waitTime(1000);
+                        message.success("Order submitted");
                         location = location;
                     }}
                     key='buy'>
                     Buy
-                </a>,
+                </Button>,
+                <Button type="primary"
+                    onClick = {async () => {
+                        console.log(record);
+                        handleModalVisible(true);
+                        handleCardUid(record.card_doc_ref);
+                        // await test(record.card_doc_ref, sellAmount, sellPrice);
+                        // await sellCard(record.card_doc_ref, sellAmount, sellPrice);
+                }}
+                key='sell'>
+                Sell
+            </Button>,
             ],
         }
 
@@ -140,13 +168,27 @@ const CardInventory = () => {
             })
     }
 
+    
+    async function sellCard(card_uid, sellAmount, sellPrice) {
+        await db.collection('cards')
+            .doc(card_uid)
+            .get()
+            .then(async(querySnapshot) => {
+                if(querySnapshot.empty){
+                    console.log("Cannot find the card in user inventory");
+                    return;
+                }
+                const oldCount = querySnapshot.data().Count;
+                await db.collection('cards').doc(card_uid).update({'Count': Number(oldCount) + Number(sellAmount)});
+                console.log(sellPrice);
+                await waitTime(5000);
+            });
+    }
 
     return(
+        <div className = "cards">
         <ProTable<Card>
             columns = {columns}
-            toolBarRender={() => [
-                <NewFacilityForm/>
-              ]}
             request = { async (params = {}) => {
                     await getData();
                     console.log("All Cards: ", cards)
@@ -158,6 +200,56 @@ const CardInventory = () => {
             }
             search={false}
         ></ProTable>
+        <ModalForm<{
+            amount: number;
+            price: number;
+        }>
+        title="Sell cards"
+        visible={createModalVisible}
+        onVisibleChange={handleModalVisible}
+        modalProps={{
+            onCancel: () => console.log('run'),
+        }}
+        onFinish={async (values) => {
+            await waitTime(1000);
+            await sellCard(card_uid, Number(values.amount), Number(values.price));
+            console.log(card_uid);
+            message.success('Order submitted');
+            location = location;
+            return true;
+        }}
+        >
+        <ProForm.Group>
+            <ProFormText
+            rules={[
+                {
+                required: true,
+                // type: 'integer',
+                },
+            ]}
+            width="md"
+            name="amount"
+            label="Amount to sell"
+            tooltip="Enter the number of cards to sell"
+            placeholder="e.g.123"
+            />
+
+        <ProFormText
+            rules={[
+                {
+                required: true,
+                // type: 'integer',
+                },
+            ]}
+            width="md"
+            name="price"
+            label="Price to sell per card (Unit: USD)"
+            tooltip="Enter the price"
+            placeholder="e.g.123"
+            /> 
+        </ProForm.Group>
+    </ModalForm>
+    </div>
     )
 };
 
