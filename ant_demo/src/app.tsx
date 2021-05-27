c99f5dc2901581ddf1ce503c2c3f318a76437aeb
Zhongman Zhang <zhongman.zhang@gmail.com>
2021-05-26 23:13:49 -0400

    added spec

--- ant_demo/src/app.tsx
+++ ant_demo/src/app.tsx
@@ -10,7 +10,7 @@ import type { ResponseError } from 'umi-request';
 import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import { BookOutlined, LinkOutlined } from '@ant-design/icons';
 
-/** 获取用户信息比较慢的时候会展示一个 loading */
+
 export const initialStateConfig = {
   loading: <PageLoading />,
 };
@@ -32,7 +32,7 @@ export async function getInitialState(): Promise<{
     }
     return undefined;
   };
-  // 如果是登录页面，不执行
+
   if (history.location.pathname !== '/user/login') {
     const currentUser = await fetchUserInfo();
     return {
@@ -55,57 +55,36 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
     footerRender: () => <Footer />,
     onPageChange: () => {
       const { location } = history;
-      // 如果没有登录，重定向到 login
+     
+      
       if (!initialState?.currentUser && location.pathname !== '/user/login') {
         history.push('/user/login');
       }
     },
-    // links: [
-    //   <>
-    //     <LinkOutlined />
-    //     <span
-    //       onClick={() => {
-    //         window.open('/umi/plugin/openapi');
-    //       }}
-    //     >
-    //       openAPI 文档
-    //     </span>
-    //   </>,
-    //   <>
-    //     <BookOutlined />
-    //     <span
-    //       onClick={() => {
-    //         window.open('/~docs');
-    //       }}
-    //     >
-    //       业务组件文档
-    //     </span>
-    //   </>,
-    // ],
+    
     menuHeaderRender: undefined,
-    // 自定义 403 页面
-    // unAccessible: <div>unAccessible</div>,
+   
     ...initialState?.settings,
   };
 };
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: '',
+  201: '',
+  202: '',
+  204: '',
+  400: '',
+  401: '',
+  403: '',
+  404: '',
+  405: '',
+  406: '',
+  410: '',
+  422: '',
+  500: '',
+  502: '',
+  503: '',
+  504: '',
 };
 
 /** 异常处理程序
@@ -118,15 +97,15 @@ const errorHandler = (error: ResponseError) => {
     const { status, url } = response;
 
     notification.error({
-      message: `请求错误 ${status}: ${url}`,
+      message: `overtime ${status}: ${url}`,
       description: errorText,
     });
   }
 
   if (!response) {
     notification.error({
-      description: '您的网络发生异常，无法连接服务器',
-      message: '网络异常',
+      description: 'Network Error, connet to the server failed',
+      message: 'Network Error',
     });
   }
   throw error;
